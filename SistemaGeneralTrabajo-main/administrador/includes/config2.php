de72e244776a9b5a9221b4d4945a233d61bf3a91
Sebastian <sebanstaiger7@gmail.com>
2023-03-13 09:17:39 -0600

    Cambios

--- SistemaGeneralTrabajo-main/administrador/includes/config2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  define( 'DB_HOST2', 'localhost' );       // Host de la BD
-  define( 'DB_USER2', 'root' );            // Usuario de la BD
-  define( 'DB_PASS2', '' );          // Password de la BD
-  define( 'DB_NAME2', 'pruebaquejas' ); // Nombre de la BD
-
-?>
